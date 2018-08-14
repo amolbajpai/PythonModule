bae53b60eae403c34aea951cbc9978b96bd822d3
Amol Bajpai <amolbajpai20@gmail.com>
2018-08-14 13:54:50 +0530

    Add report item to report list

--- GPS_module.py
+++ GPS_module.py
@@ -85,3 +85,4 @@ def find_latest_current_status_report_old():
     report_list=[]
     for i in os.listdir('/home/amol/Downloads/'):
         if "Current_Status_Report" in i:
+            report_list.append(i)
