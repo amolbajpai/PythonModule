40cc47ba6945fca502579edd016a5945ada017f8
Amol Bajpai <amolbajpai20@gmail.com>
2019-04-04 13:55:11 +0530

    Added GPS current status report extraction.

--- GPS_module.py
+++ GPS_module.py
@@ -205,3 +205,4 @@ def GPS_Email_Report():
     from openpyxl import load_workbook
 
     gps = load_workbook('/home/amol/Documents/Excel Files/Email GPS Ver 5.xlsx')
+    gps_ws=gps['Current Status Report']
