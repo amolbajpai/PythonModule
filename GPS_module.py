162df4a24dbbd1286d19393e17f5314b2c4dd518
Amol Bajpai <amolbajpai20@gmail.com>
2019-04-26 13:55:13 +0530

    Added iteration loop over max columns in novire_ws

--- GPS_module.py
+++ GPS_module.py
@@ -215,3 +215,4 @@ def GPS_Email_Report():
     gps_ws.delete_cols(1,20) # to delete previous data of the first 1 to 20 columns
 
     for i in range(1,novire_ws.max_row+1): # novire_ws.max_column+1
+        for j in range(1,novire_ws.max_column+1):
