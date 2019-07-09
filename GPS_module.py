ebd09f3afd350333fea6847d437128bc81ef20c9
Amol Bajpai <amolbajpai20@gmail.com>
2019-07-09 13:55:21 +0530

    Renamed Vehicle No to Vehicle and Vehicle control Location to in Varuna columns in vsr DataFrame

--- GPS_module.py
+++ GPS_module.py
@@ -258,3 +258,4 @@ def find_changes_in_controling_branch():
     Basic_Info_from_CSR.rename(columns= {'Controlling Branch' : 'On Novire'},inplace=True)
 
     vsr= pd.read_excel(path_last_vsr,sheet_name="Vehicle_Status_Register_Quick",usecols=('Vehicle No','Vehicle control Location'))
+    vsr.rename(columns={'Vehicle No' :'Vehicle', 'Vehicle control Location' : 'in Varuna' },inplace=True)
