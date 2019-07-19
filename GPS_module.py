5029cb8573d1bbbedc3da7448c29e646ad0ebb53
Amol Bajpai <amolbajpai20@gmail.com>
2019-07-19 13:55:22 +0530

    `Added line to extract mismatched values from final list`

--- GPS_module.py
+++ GPS_module.py
@@ -262,3 +262,4 @@ def find_changes_in_controling_branch():
     vsr=vsr[['Vehicle','in Varuna']]
     final = Basic_Info_from_CSR.merge(vsr,on='Vehicle')
     filt = final['On Novire'] == final['in Varuna']
+    mismatched = final[-filt]
