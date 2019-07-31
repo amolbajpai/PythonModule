3e009fd4f2d06c2e30536801d288dd515c9f63df
Amol Bajpai <amolbajpai20@gmail.com>
2019-07-31 13:55:23 +0530

    Added subprocess call to open Update Controlling Branch.xlsx using et

--- GPS_module.py
+++ GPS_module.py
@@ -267,3 +267,4 @@ def find_changes_in_controling_branch():
     mismatched['SrNo']= range(1,mismatched['Vehicle'].count()+1,1)
     mismatched=mismatched[['SrNo','Vehicle','Old Controlling Branch','Current Controlling Branch']]
     gps.df_to_excel('/home/amol/Documents/Excel Files/Update Controlling Branch Template.xlsx','Sheet1',row=2,col=1,df=mismatched,output='/home/amol/Desktop/Update Controlling Branch.xlsx')
+    subprocess.call(["et",'/home/amol/Desktop/Update Controlling Branch.xlsx'],shell=False)
