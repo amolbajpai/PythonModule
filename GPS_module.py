a83012274b517f56b17b7c7c8cbfd3db1d48bcb2
Amol Bajpai <amolbajpai20@gmail.com>
2019-10-17 13:55:32 +0530

    `Added functionality to recursively collect files from subdirectories`

--- GPS_module.py
+++ GPS_module.py
@@ -300,3 +300,4 @@ def getListOfFiles(dirName):
         fullPath = os.path.join(dirName, entry)
         # If entry is a directory then get the list of files in this directory
         if os.path.isdir(fullPath):
+            allFiles = allFiles + getListOfFiles(fullPath)
