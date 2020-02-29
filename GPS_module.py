7b4973775b2e5199fd32e265fd59ca2434d7ded6
Amol Bajpai <amolbajpai20@gmail.com>
2020-02-29 13:55:47 +0530

    `Added code to load workbook for vehicle hold in other fleet`

--- GPS_module.py
+++ GPS_module.py
@@ -364,3 +364,4 @@ def gps_stop_enroute_report():
     gps_ws=gps['Current Status Report']
     novire = load_workbook("/home/amol/Downloads/temp.xlsx")
     novire_ws=novire['Sheet1']
+    #vhiof = load_workbook('/home/amol/Desktop/VHIOF/vehicle hold in other fleet.xlsx')
