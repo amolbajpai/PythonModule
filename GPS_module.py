360bddc1775af48c52c546d28ba6472a36d80ec6
Amol Bajpai <amolbajpai20@gmail.com>
2020-04-02 13:55:49 +0530

    Copied cell values from vhiof_ws to gps_ws_pastereport.

--- GPS_module.py
+++ GPS_module.py
@@ -378,3 +378,4 @@ def gps_stop_enroute_report():
 
     #for i in range(1,vhiof_ws.max_row+1): # novire_ws.max_column+1
     #    for j in range(1,vhiof_ws.max_column+1):
+    #        gps_ws_pastereport.cell(row=i,column=j).value=vhiof_ws.cell(row=i,column=j).value
