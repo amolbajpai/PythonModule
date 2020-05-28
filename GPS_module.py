fda3a12498df6a4a32e9339560ae2667af4082ff
Amol Bajpai <amolbajpai20@gmail.com>
2020-05-28 13:55:56 +0530

    Added driver mobile number to GPS report info in Excel

--- GPS_module.py
+++ GPS_module.py
@@ -407,3 +407,4 @@ def gps_stop_enroute_report():
     for i in vsr.index:
         gps_report_info.cell(row=r,column=6).value=vsr.iloc[r-1,0]
         gps_report_info.cell(row=r,column=7).value=vsr.iloc[r-1,1]
+        gps_report_info.cell(row=r,column=10).value=vsr.iloc[r-1,2] #for driver mobile number add in excel
