013702dc0f24b872bd5097f1d20280643c282a1a
Amol Bajpai <amolbajpai20@gmail.com>
2021-02-13 13:56:24 +0530

    Added loop to wait for Current Status Report image to appear on screen

--- GPS_module.py
+++ GPS_module.py
@@ -535,3 +535,5 @@ def download_current_status_report_gui():
             pass
     pyautogui.moveTo(pyautogui.locateCenterOnScreen('SelectReport.png',confidence=0.9),duration=0)
     pyautogui.click()
+
+    while pyautogui.locateCenterOnScreen('CurrentStatusReport.png',confidence=0.99) is None:
