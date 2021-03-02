e18098b1212f222c822c8d00fb3092877b79228d
Amol Bajpai <amolbajpai20@gmail.com>
2021-03-02 13:56:26 +0530

    Added pyautogui functionality to locate and move to Download button on screen

--- GPS_module.py
+++ GPS_module.py
@@ -543,3 +543,4 @@ def download_current_status_report_gui():
 
     while pyautogui.locateCenterOnScreen('Download.png',confidence=0.99) is None:
             pass
+    pyautogui.moveTo(pyautogui.locateCenterOnScreen('Download.png',confidence=0.9),duration=0)
