68b0feb331512d8d431ca58478f62cf3680b733a
Amol Bajpai <amolbajpai20@gmail.com>
2021-03-09 13:56:26 +0530

    Added retry logic to locate downloaded CSR Excel file on screen with 90% confidence

--- GPS_module.py
+++ GPS_module.py
@@ -545,3 +545,5 @@ def download_current_status_report_gui():
             pass
     pyautogui.moveTo(pyautogui.locateCenterOnScreen('Download.png',confidence=0.9),duration=0)
     pyautogui.click()
+
+    while pyautogui.locateCenterOnScreen('downloaded_csr_excel_file.png',confidence=0.9) is None:
