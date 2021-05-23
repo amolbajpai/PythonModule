540869cd68c69b5f81153b169df686eaea4f1304
Amol Bajpai <amolbajpai20@gmail.com>
2021-05-23 13:56:33 +0530

    Added control branches list with specific codes

--- GPS_module.py
+++ GPS_module.py
@@ -579,3 +579,4 @@ def send_gps_report_email_gui():
     #controling_branchs=["03-FLTBLE"]
 
     #BLY + HYD + KOL
+    #controling_branchs=["01-FLBWSE","02-FLTBLS","03-FLTBLE","04-FLBNSE","05-FLTBNW","06-FLBNWS","08-HYD","09-KOL"]
