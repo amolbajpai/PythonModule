3d9a4480d600241ea240eae6c41297e4c2787ac9
Amol Bajpai <amolbajpai20@gmail.com>
2021-06-13 13:56:36 +0530

    Added placeholder code block for future implementation

--- GPS_module.py
+++ GPS_module.py
@@ -593,3 +593,4 @@ def send_gps_report_email_gui():
         pyautogui.click()
         pyautogui.typewrite(['left'], interval=0.2)
         while pyautogui.locateCenterOnScreen('list_popup.png',confidence=0.99) is None:
+            pass
