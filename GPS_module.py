7104b5f28bab82fcccf3abd398f6273b35bdd1b0
Amol Bajpai <amolbajpai20@gmail.com>
2021-08-13 13:56:44 +0530

    Added functionality to paste clipboard contents using Ctrl+V hotkey via pyautogui

--- GPS_module.py
+++ GPS_module.py
@@ -626,3 +626,4 @@ def send_gps_report_email_gui():
             pass
 
         #pyautogui.moveTo(pyautogui.locateCenterOnScreen('ThunderbirdSendButton.png',confidence=0.9),duration=0)
+        pyautogui.hotkey('ctrl', 'v')
