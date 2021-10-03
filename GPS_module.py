b4436c9c05d594b1fcd70c05151de6da1bda58e0
Amol Bajpai <amolbajpai20@gmail.com>
2021-10-03 13:56:49 +0530

    Added pyautogui function to move to Create Email button on the screen.

--- GPS_module.py
+++ GPS_module.py
@@ -649,3 +649,4 @@ def send_gps_report_email_gui():
     def Send_Email_Now():
         while pyautogui.locateCenterOnScreen('CreateEmail.png',confidence=0.99) is None:
             pass
+        pyautogui.moveTo(pyautogui.locateCenterOnScreen('CreateEmail.png',confidence=0.9),duration=0)
