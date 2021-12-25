186006a22b24371d53557cc3898cffb00b956702
Amol Bajpai <amolbajpai20@gmail.com>
2021-12-25 13:56:58 +0530

    Added wait loop for Dear Concern image detection with 80% confidence

--- GPS_module.py
+++ GPS_module.py
@@ -693,3 +693,4 @@ def send_gps_report_email_gui():
                 if Found_Blank():
                     print('Report is blank')
                 else:
+                    while pyautogui.locateCenterOnScreen('DearConcern.png',confidence=0.8) is None:
