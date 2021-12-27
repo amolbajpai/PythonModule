1e318fb946491c8c49b996a1cf2c9317155e3f7c
Amol Bajpai <amolbajpai20@gmail.com>
2021-12-27 13:56:59 +0530

    Added pyautogui functionality to locate and move to DearConcern.png image on screen

--- GPS_module.py
+++ GPS_module.py
@@ -695,3 +695,4 @@ def send_gps_report_email_gui():
                 else:
                     while pyautogui.locateCenterOnScreen('DearConcern.png',confidence=0.8) is None:
                         pass
+                    pyautogui.moveTo(pyautogui.locateCenterOnScreen('DearConcern.png',confidence=0.8),duration=0)
