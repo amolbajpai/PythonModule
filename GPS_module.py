3d0b2c9bd115372945d7e578ebdfe5b6827ed188
Amol Bajpai <amolbajpai20@gmail.com>
2022-07-13 13:57:26 +0530

    Added dynamic target file name generation based on clipboard data

--- GPS_module.py
+++ GPS_module.py
@@ -801,3 +801,4 @@ def route_geofence_validation(clipboard_data):
         final = final.loc[start_index:end_index:]
         final = final[['SrNo', 'Vehicle', 'Date/Time', 'Location', 'Latitude', 'Longitude', 'Speed']]
         final['SrNo']=range(1,final['SrNo'].count()+1,1)
+        target_file_name = '/home/amol/Documents/Excel Files/Geofance/output/Route Geo-fence for '+clipboard_data['From Location'][0].title()+' to '+clipboard_data['To Location'][0].title()+'.xlsx'
