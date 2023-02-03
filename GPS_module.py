1cc9568d109ef68bc586fdb55219b8cf05ed2f29
Amol Bajpai <amolbajpai20@gmail.com>
2023-02-03 13:57:49 +0530

    Extracted substring after specific character in test field of final dataframe

--- GPS_module.py
+++ GPS_module.py
@@ -901,3 +901,4 @@ def route_geofence_validation(clipboard_data):
         #print(final.loc[i,'test'])
         if To_point in final.loc[i,'test']:
             found_after_char = final.loc[i,'test'].find(To_point)
+            temp_location = final.loc[i,'test'][found_after_char:]
