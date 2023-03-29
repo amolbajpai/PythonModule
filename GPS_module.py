f174b39038a388e1abdeab646b7df00cc08a507c
Amol Bajpai <amolbajpai20@gmail.com>
2023-03-29 13:57:56 +0530

    Added exclusion location check for test locations in final.loc array

--- GPS_module.py
+++ GPS_module.py
@@ -929,3 +929,4 @@ def route_geofence_validation(clipboard_data):
     for i in final[::-1].index:
         exclude_flag = 0
         for excl_loc in exclude_locations:
+            if excl_loc in final.loc[i,'test']:
