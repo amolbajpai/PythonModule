c5ee5345215d45507f8b54ea4173ba41878dd38c
Amol Bajpai <amolbajpai20@gmail.com>
2023-04-19 13:57:59 +0530

    Added iteration over enroute keywords

--- GPS_module.py
+++ GPS_module.py
@@ -940,3 +940,4 @@ def route_geofence_validation(clipboard_data):
             found_after_char = final.loc[i,'test'].find(From_point)
             temp_location = final.loc[i,'test'][found_after_char:]
             found_flag = 0
+            for e_location in enroute_keywords:
