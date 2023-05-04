089bebee7954e9fc874f7eb8fdb71ddc553dc049
Amol Bajpai <amolbajpai20@gmail.com>
2023-05-04 13:58:01 +0530

    Added flag variable to track found status

--- GPS_module.py
+++ GPS_module.py
@@ -947,3 +947,4 @@ def route_geofence_validation(clipboard_data):
 
                #to exclude 'KM Away From'
                 elif 'km away from' in final.loc[i,'test']:
+                    found_flag = 1
