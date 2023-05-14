31c1cab6a9320c72161e4e5bfaa6128d6d1cbfcc
Amol Bajpai <amolbajpai20@gmail.com>
2023-05-14 13:58:02 +0530

    Added break statement to exit loop prematurely

--- GPS_module.py
+++ GPS_module.py
@@ -953,3 +953,4 @@ def route_geofence_validation(clipboard_data):
 
             if found_flag == 0: # then location is ok
                 end_index = i
+                break #commenting to get last from point
