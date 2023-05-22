49ba9c032da9a5a4db80fd2c8b156ebe6bce57cf
Amol Bajpai <amolbajpai20@gmail.com>
2023-05-22 13:58:03 +0530

    `Added filter application to retrieve specific final values`

--- GPS_module.py
+++ GPS_module.py
@@ -957,3 +957,4 @@ def route_geofence_validation(clipboard_data):
             else:
                 end_index = -1
     """filt = (final['test'].str.contains(From_point,na=False)) & ~(final['test'].str.contains('road',na=False)) & ~(final['test'].str.contains('expressway',na=False)) & ~(final['test'].str.contains('km away from',na=False))  # KM Away from
+    e=final[filt]
