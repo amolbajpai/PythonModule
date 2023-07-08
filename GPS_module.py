0061013ee3277161884c4e01548f45dba86d8dba
Amol Bajpai <amolbajpai20@gmail.com>
2023-07-08 13:58:09 +0530

    Added loop to iterate through route_city_validated data

--- GPS_module.py
+++ GPS_module.py
@@ -983,3 +983,4 @@ def route_geofence_validation(clipboard_data):
     else:
         problem = ''
         final = final.loc[start_index:end_index:].copy()   
+        for i in route_city_validated:
