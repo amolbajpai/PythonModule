4befa6e53cef09e7b5418b181718eb506ad99957
Amol Bajpai <amolbajpai20@gmail.com>
2023-09-30 13:58:20 +0530

    Added mouse movement to Real Time Report dropdown using pyautogui

--- GPS_module.py
+++ GPS_module.py
@@ -1025,3 +1025,4 @@ def download_real_time_report(clipboard_data):
     #detect Real Time Drop Down 
     while pyautogui.locateCenterOnScreen('RealTimeReportDropDown.png',confidence=0.8) is None:
             pass
+    pyautogui.moveTo(pyautogui.locateCenterOnScreen('RealTimeReportDropDown.png',confidence=0.8),duration=0)
