77983af4f731f499f368d9100a0a156227a30e7e
Amol Bajpai <amolbajpai20@gmail.com>
2023-11-17 13:58:27 +0530

    I dont see any code in your message. Please paste the code youve added, and Ill be happy to help you generate a commit message in a single line.

--- GPS_module.py
+++ GPS_module.py
@@ -1050,3 +1050,4 @@ def download_real_time_report(clipboard_data):
             pass
     pyautogui.moveTo(pyautogui.locateCenterOnScreen('Download_button.png',confidence=0.8),duration=0)
     pyautogui.moveRel(xOffset=0,yOffset=-30)
+    
