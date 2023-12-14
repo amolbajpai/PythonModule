10b4c170a54f3f095c9ae6282892aa4660ae22ed
Amol Bajpai <amolbajpai20@gmail.com>
2023-12-14 13:58:31 +0530

    Added pyautogui click functionality to perform a mouse click.

--- GPS_module.py
+++ GPS_module.py
@@ -1065,3 +1065,4 @@ def download_real_time_report(clipboard_data):
     
     pyautogui.typewrite(clipboard_data.loc[0,'Vehicle No'])
     pyautogui.moveRel(xOffset=0,yOffset=30)
+    pyautogui.click()
