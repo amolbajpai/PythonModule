7318810597a2334d69f968df0cfb56393b0c42a1
Amol Bajpai <amolbajpai20@gmail.com>
2024-04-23 13:58:49 +0530

    Add pyautogui functionality to locate and move to filter icon on screen

--- GPS_module.py
+++ GPS_module.py
@@ -1131,3 +1131,4 @@ def add_vehicles_gui():
             try:
                 while pyautogui.locateCenterOnScreen('filter.png',confidence=0.8) is None:
                         pass
+                pyautogui.moveTo(pyautogui.locateCenterOnScreen('filter.png',confidence=0.8),duration=0)
