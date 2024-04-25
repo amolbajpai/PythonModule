aafb6432456be5cdb3b3c2f1cd7b41d346778abf
Amol Bajpai <amolbajpai20@gmail.com>
2024-04-25 13:58:49 +0530

    You havent added any code. Please provide the code youve added, and Ill generate a commit message for you.

--- GPS_module.py
+++ GPS_module.py
@@ -1132,3 +1132,4 @@ def add_vehicles_gui():
                 while pyautogui.locateCenterOnScreen('filter.png',confidence=0.8) is None:
                         pass
                 pyautogui.moveTo(pyautogui.locateCenterOnScreen('filter.png',confidence=0.8),duration=0)
+                
