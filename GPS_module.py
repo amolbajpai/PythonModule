ccd37bfc78d942e16f92638926bbe3d271762353
Amol Bajpai <amolbajpai20@gmail.com>
2024-04-29 13:58:50 +0530

    Added bare except block to handle unhandled exceptions.

--- GPS_module.py
+++ GPS_module.py
@@ -1134,3 +1134,4 @@ def add_vehicles_gui():
                 pyautogui.moveTo(pyautogui.locateCenterOnScreen('filter.png',confidence=0.8),duration=0)
                 
                 break
+            except:
